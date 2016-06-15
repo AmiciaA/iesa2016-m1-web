e2732688a063016ffc7ddb905f478642ba2acf7a
OphelieF <ophefaivre@gmail.com>
2016-06-15 14:24:51 +0200

    #2 Variable et Constante

---
+++ swift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello")
+
+//var myVariable = 42
+
+//myVariable = 50
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble :Double = 70
+
