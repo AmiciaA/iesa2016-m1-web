5da675d6adeaeeb8bb2687b2e8e041446a17830c
OphelieF <ophefaivre@gmail.com>
2016-06-15 15:03:28 +0200

    closes #2 array and dico

--- swift/MyPlayground.playground/Contents.swift
+++ swift/MyPlayground.playground/Contents.swift
@@ -14,3 +14,33 @@ let implicitInteger = 70
 let implicitDouble = 70.0
 let explicitDouble :Double = 70
 
+let label = "The width is"
+
+let width = 94
+
+let widthLabel = label + String(width)
+
+let apples = 3
+
+let oranges = 5
+
+let appleSummary = "I have \(apples) apples."
+
+//let fruits = I have \(apples + oranges) fruits"
+
+
+var ShopL = ["cat", "dog", "water"]
+print(ShopL[1])
+ShopL[1] = "rat"
+print(ShopL)
+//ShopL[10] = "bla"
+//ShopL[2] = 22
+
+var emptyArray = [String]()
+emptyArray.append("Hello")
+//emptyArray.append(33)
+let emptyDico = [String : Float]()
+
+var emptyArray2 = []
+//emptyArray2.append("Hello")
+//emptyArray2.append(33)
